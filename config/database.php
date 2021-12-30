8d8a5014aa6d7f5a0aa6775c65fac18dcc61eae3
Mark Jacob D <mjacob.doctor@gmail.com>
2021-12-30 22:28:37 +0800

    updates

--- config/database.php
+++ config/database.php
@@ -14,8 +14,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    'default' => env('DB_CONNECTION', 'mysql'),
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    // 'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/js/Pages/Task/Task.vue
+++ resources/js/Pages/Task/Task.vue
@@ -48,6 +48,8 @@
 
         </template>
 
+
+
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
@@ -81,74 +83,60 @@
                         </draggable>
 
                     </table>
+​
+                    <dialog-modal :show="isOpen" @close="openModal(false)" >
+                        <template #title>
+                            Add new task
+                        </template>
 
-                    <div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400" v-if="isOpen">
-                      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <template #content>
+                        <!-- Name -->
+                            <div class="mb-4">
+                                <jet-label for="name" value="Name" />
+                                <jet-input id="name" ref="taskname" type="text" class="mt-1 block w-full" @keyup.enter="save" v-model="form.name"/>
 
-                        <div class="fixed inset-0 transition-opacity">
-                          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-                        </div>
+                            </div>
 
-                        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
-                            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-                                role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                            <!-- Description -->
+                            <div class="mb-4">
+                                <jet-label for="description" value="Description" @keyup.enter="save" />
+                                <jet-input id="description" type="text" class="mt-1 block w-full" v-model="form.description" />
+                            </div>
 
-                                <form>
-                                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                        <!-- Name -->
-                                            <div class="mb-4">
-                                                <jet-label for="name" value="Name" />
-                                                <jet-input id="name" ref="taskname" type="text" class="mt-1 block w-full" v-model="form.name"/>
-                                                <!-- <jet-input-error :message="$page.props.errors.name" class="mt-2" /> -->
-                                            </div>
+                            <!-- Status -->
+                            <div class="mb-4" v-show="editMode">
+                                <jet-label for="status" value="Status" />
+                                <select ref="status" id="status" class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                    v-model="form.status">
+                                    <option v-for="value in options" :key="value" :value="value">{{value}}</option>
+                                </select>
+                            </div>
 
-                                            <!-- Description -->
-                                            <div class="mb-4">
-                                                <jet-label for="description" value="Description" />
-                                                <jet-input id="description" type="text" class="mt-1 block w-full" v-model="form.description" />
-                                            </div>
+                            <!-- Custom Status -->
+                            <div class="mb-4" v-show="form.status == 'Custom'">
+                                <jet-label for="customstatus" value="" />
+                                <jet-input id="customstatus" type="text" class="mt-1 block w-full" v-model="form.custom_status" />
+                            </div>
+                        </template>
 
-                                            <!-- Status -->
-                                            <div class="mb-4" v-show="editMode">
-                                                <jet-label for="status" value="Status" />
-                                                <select ref="status" id="status" class="mt-1 block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-                                                    v-model="form.status">
-                                                    <option v-for="value in options" :key="value" :value="value">{{value}}</option>
-                                                </select>
-                                            </div>
+                        <template #footer>
 
-                                            <!-- Custom Status -->
-                                            <div class="mb-4" v-show="form.status == 'Custom'">
-                                                <jet-label for="customstatus" value="" />
-                                                <jet-input id="customstatus" type="text" class="mt-1 block w-full" v-model="form.custom_status" />
-                                            </div>
-                                    </div>
+                            <jet-secondary-button @click="openModal(false)" >
+                                Cancel
+                            </jet-secondary-button>
 
-                                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                                        <button :disabled="disabled" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                                                v-show="!editMode" @click="save(form)">
-                                            Save
-                                        </button>
-                                        </span>
-                                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                                        <button :disabled="disabled" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                                                    v-show="editMode" @click="update(form)">
-                                            Update
-                                        </button>
-                                        </span>
-                                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                            <jet-button class="ml-2"
+                                    v-show="!editMode" @click="save(form)" :class="{ 'opacity-25': disabled}" :disabled="disabled">
+                                Save
+                            </jet-button>
 
-                                        <button @click="openModal(false)" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                                            Cancel
-                                        </button>
-                                        </span>
-                                    </div>
 
-                                </form>
-                        </div>
-                      </div>
-                    </div>
+                            <jet-button class="ml-2"  :class="{ 'opacity-25': disabled}" :disabled="disabled"
+                                        v-show="editMode" @click="update(form)">
+                                Update
+                            </jet-button>
+                        </template>
+                    </dialog-modal>
                 </div>
             </div>
         </div>
